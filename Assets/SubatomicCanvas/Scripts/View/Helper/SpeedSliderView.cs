f06cf750e953896c7c9688eb60abd7357d301772
VMelville <43390710+VMelville@users.noreply.github.com>
2023-07-22 23:53:02 +0900

    Slider handle icon now changes according to speed.

--- Assets/SubatomicCanvas/Scripts/View/Helper/SpeedSliderView.cs
+++ Assets/SubatomicCanvas/Scripts/View/Helper/SpeedSliderView.cs
@@ -1,4 +1,3 @@
-using SubatomicCanvas.Model;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -9,29 +8,29 @@ namespace SubatomicCanvas.View
     {
         [SerializeField] private Slider slider;
 
-        [SerializeField] private GameObject fastForwardIcon;
-        [SerializeField] private GameObject playIcon;
-        [SerializeField] private GameObject pauseIcon;
-        [SerializeField] private GameObject reverseIcon;
-        [SerializeField] private GameObject fastRewindIcon;
+        [SerializeField] private Image icon;
+        
+        [SerializeField] private Sprite fastForwardSprite;
+        [SerializeField] private Sprite playSprite;
+        [SerializeField] private Sprite pauseSprite;
+        [SerializeField] private Sprite reverseSprite;
+        [SerializeField] private Sprite fastRewindSprite;
 
         public UnityEvent<float> onValueChanged => slider.onValueChanged;
         
         private const float Threshold = 1.5f;
         
-        private void Start()
-        {
-            Debug.LogWarning("ToDo: スライダーのハンドル画像をスライダー位置によって変える");
-        }
-
         public void SetSpeed(float speed)
         {
-            fastForwardIcon.SetActive(speed > Threshold);
-            playIcon       .SetActive(speed is > 0.0f and <= Threshold);
-            pauseIcon      .SetActive(speed == 0.0f);
-            reverseIcon    .SetActive(speed is < 0.0f and >= -Threshold);
-            fastRewindIcon .SetActive(speed < -Threshold);
-            
+            icon.sprite = speed switch
+            {
+                > Threshold              => fastForwardSprite,
+                > 0.0f and <= Threshold  => playSprite,
+                0.0f                     => pauseSprite,
+                < 0.0f and >= -Threshold => reverseSprite,
+                < -Threshold             => fastRewindSprite,
+                _                        => icon.sprite
+            };
         }
     }
 }
